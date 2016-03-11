9f06b6b60f8c7e621cfe2934299b1ce2d73ab2be
cassidoo <cassidy@clarifai.com>
2016-03-11 12:06:54 -0500

    Reformat content, get vars for greeting

--- content.js
+++ content.js
@@ -1,34 +1,35 @@
-function githubURL() {
-  var url = 'https://api.github.com/repos/cassidoo/cassidoo-v4/contents/contents/press.txt';
-  return function() {
-    return url;
-  };
-}
-
 function pullInfo() {
-  fetch('contents/info.json').then(function(response) {
+  return fetch('contents/info.json').then(function(response) {
     return response.json();
-  }).then(function(j) {
-    console.log(j);
   });
 }
 
 function pullPress() {
   fetch('contents/press.json').then(function(response) {
     return response.json();
-  }).then(function(j) {
-    console.log(j);
   });
 }
 
-function parseInfo() {
+function greetingVars() {
+  var title = '';
+  var company = '';
+  var fulltime = [];
+  var intern = [];
+  var hobbies = [];
 
+  pullInfo().then(function(r) {
+    var x = r.data;
+    title = x.title;
+    company = x.company;
+    fulltime = x.past.fulltime;
+    intern = x.past.intern;
+    hobbies = x.hobbies;
+  });
 }
-k
-function parsePress() {
 
-}
+function pressVars() {
+  var recent = [];
+  pullPress().then(function(r) {
 
-function greeting() {
-  // TODO: template literals?
+  });
 }
