f318bb80c8582f311af9d644316f1ffd820909b0
cassidoo <cassidy@clarifai.com>
2016-03-25 23:04:56 -0400

    Refactor JSON parsing, set up template literal for intro

--- content.js
+++ content.js
@@ -14,19 +14,18 @@ function pullPress() {
 
 // get variable values for About section
 function infoVars() {
-  var title = '';
-  var company = '';
-  var fulltime = [];
-  var intern = [];
-  var hobbies = [];
+  var info = {};
 
   pullInfo().then(function(r) {
     var x = r.data;
-    title = x.title;
-    company = x.companies.fulltime[0];
-    fulltime = x.companies.fulltime; // TODO: everything but first element
-    intern = x.past.intern;
-    hobbies = x.hobbies;
+    info.title = x.title;
+    info.company = x.companies.fulltime[0];
+    info.fulltime = x.companies.fulltime.slice(1);
+    info.intern = x.companies.intern;
+    info.hobbies = x.hobbies;
+  })
+  .then(function() {
+    intro(info);
   });
 }
 
@@ -34,8 +33,16 @@ function infoVars() {
 function pressVars() {
   var recent = [];
   pullPress().then(function(r) {
-
   });
+}
+
+function intro(info) {
+  var content = document.getElementById('content');
+  content.innerHTML = html(`
+    <div class="intro">
+        Hi, My name is Cassidy Williams and I am a ${info.title}.
+    </div>
+  `);
 }
 
 // escape HTML characters in template literals
@@ -48,6 +55,8 @@ function htmlEscape(str) {
     .replace(/`/g, '&#96;');
 }
 
+// set up HTML templating
+// based on: http://www.2ality.com/2015/01/template-strings-html.html
 function html(literalSections, ...substs) {
   // use raw literal sections, don't interpret backslashes
   var raw = literalSections.split("\\");
@@ -58,12 +67,12 @@ function html(literalSections, ...substs) {
     var lit = raw[i];
 
     // if substitution is an array (and not a string), turn it into a string
-    if(Array.isArray(subst)) {
+    if (Array.isArray(subst)) {
       subst = subst.join('');
     }
 
     // if substitution is preceded by a dollar sign, we escape special characters
-    if(lit.endsWith('$')) {
+    if (lit.endsWith('$')) {
       subst = $tmpl.html_escape(subst);
       lit = lit.slice(0, -1);
     }
@@ -72,8 +81,6 @@ function html(literalSections, ...substs) {
   });
 
   result += raw[raw.length-1];
-
   return result;
 }
-
 
